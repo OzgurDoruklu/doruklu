62922b539fd0ba665f84356184543f19cda2e359
OzgurDoruklu <ozgurdoruklu@gmail.com>
2020-08-14 11:12:44 +0300

    Updates

---
+++ precache-manifest.4897cbba9181db1c4de402e0a259996a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26da8c87dd630b9fb5aea3dc849af9c",
+    "url": "/doruklu/index.html"
+  },
+  {
+    "revision": "d80c2308747ea12d971f",
+    "url": "/doruklu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6082b327d09ce7eeefb1",
+    "url": "/doruklu/static/js/2.a54ea5f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/doruklu/static/js/2.a54ea5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d80c2308747ea12d971f",
+    "url": "/doruklu/static/js/main.326aa687.chunk.js"
+  },
+  {
+    "revision": "d7e6e77dcebef75c4056",
+    "url": "/doruklu/static/js/runtime-main.b53ede1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/doruklu/static/media/logo.5d5d9eef.svg"
+  }
+]);
