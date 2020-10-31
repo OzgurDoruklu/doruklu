0358265b1411dd7c9d09780ccfd7300bc7441627
OzgurDoruklu <ozgurdoruklu@gmail.com>
2020-10-31 19:22:31 +0300

    Toprak 1 Yaşında

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>TOPRAK 1 Yaşında</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
